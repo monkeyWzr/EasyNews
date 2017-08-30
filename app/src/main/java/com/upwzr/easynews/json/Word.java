58067c29990eef2f9202447359e1496cfa900471
Administrator <monkeywzr@gmail.com>
2017-08-31 02:57:19 +0800

    add getters and setters

--- app/src/main/java/com/upwzr/easynews/json/Word.java
+++ app/src/main/java/com/upwzr/easynews/json/Word.java
@@ -7,21 +7,21 @@ import com.google.gson.annotations.SerializedName;
  */
 
 public class Word {
-    public String PinYin;
+    public String pinYin;
 
     public String PronounceJp;
 
     public String Tone;
 
     @SerializedName("Word")
-    public String text;
+    public String Text;
 
     public String Comment;
 
     public String Pronounce;
 
     @SerializedName("TtsUrl")
-    public String mp3;
+    public String Mp3Url;
 
     public boolean IsAddWord;
 
@@ -31,4 +31,91 @@ public class Word {
 
     public String ToLang;
 
+    public String getPinYin() {
+        return pinYin;
+    }
+
+    public void setPinYin(String pinYin) {
+        this.pinYin = pinYin;
+    }
+
+    public String getPronounceJp() {
+        return PronounceJp;
+    }
+
+    public void setPronounceJp(String pronounceJp) {
+        PronounceJp = pronounceJp;
+    }
+
+    public String getTone() {
+        return Tone;
+    }
+
+    public void setTone(String tone) {
+        Tone = tone;
+    }
+
+    public String getText() {
+        return Text;
+    }
+
+    public void setText(String text) {
+        Text = text;
+    }
+
+    public String getComment() {
+        return Comment;
+    }
+
+    public void setComment(String comment) {
+        Comment = comment;
+    }
+
+    public String getPronounce() {
+        return Pronounce;
+    }
+
+    public void setPronounce(String pronounce) {
+        Pronounce = pronounce;
+    }
+
+    public String getMp3Url() {
+        return Mp3Url;
+    }
+
+    public void setMp3Url(String mp3Url) {
+        Mp3Url = mp3Url;
+    }
+
+    public boolean isAddWord() {
+        return IsAddWord;
+    }
+
+    public void setAddWord(boolean addWord) {
+        IsAddWord = addWord;
+    }
+
+    public long getWordId() {
+        return WordId;
+    }
+
+    public void setWordId(long wordId) {
+        WordId = wordId;
+    }
+
+    public String getFromLang() {
+        return FromLang;
+    }
+
+    public void setFromLang(String fromLang) {
+        FromLang = fromLang;
+    }
+
+    public String getToLang() {
+        return ToLang;
+    }
+
+    public void setToLang(String toLang) {
+        ToLang = toLang;
+    }
 }
